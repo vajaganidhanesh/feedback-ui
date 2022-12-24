98170f9a46bd3097dd5f7652b77df99a456e4039
vajagani dhanesh <vajaganidhanesh@gmail.com>
2022-12-24 10:21:07 +0530

    unique feedback was commented

--- src/ContactProvider/FeedbackContext.js
+++ src/ContactProvider/FeedbackContext.js
@@ -1,4 +1,4 @@
-// import { v4 as uuidv4 } from "uuid";
+ // import { v4 as uuidv4 } from "uuid";
 import { createContext, useEffect, useState } from "react";
 const FeedbackContext = createContext();
 
